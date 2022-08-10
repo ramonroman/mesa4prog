80778d05c05c9dc505ae232ebb62ef9674fa8fd1
ramonroman <ramonroman@users.noreply.github.com>
2022-08-10 18:29:16 -0300

    Adicionando calculadora IMC (ex4.html)

---
+++ ex2.js
@@ -0,0 +1,12 @@
+let nome="Ramon"
+let altura=1.71
+let usuarioAtivo=false
+
+console.log
+
+console.log("Nome: ");
+console.log(nome);
+console.log('Altura: ');
+console.log(altura);
+console.log("Usuário Ativo?");
+console.log(usuarioAtivo)
