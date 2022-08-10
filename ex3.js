0d484aaf6c88f3cc45bc0590c8bf97fef27d6869
ramonroman <ramonroman@users.noreply.github.com>
2022-08-09 22:17:15 -0300

    Commit aula 4 programação

---
+++ ex3.js
@@ -0,0 +1,43 @@
+//Pergunta 1
+/*
+console.log(1+2);
+*/
+
+//Pergunta 2
+/*
+console.log(5/2);
+*/
+
+/*
+//Pergunta 3 e 4
+let num1=1;
+let num2=2;
+console.log(num1+num2)
+
+let result;
+result = num1 + num2;
+console.log("A soma de num1 e num2 é:" + result);
+*/
+
+/*
+//Pergunta 5
+let num1=1+1;
+let num2=2;
+console.log(num1+num2)
+*/
+
+/*
+//Pergunta 6
+
+let num1=("Olá" + 23);
+let num2=2;
+console.log(num1+num2)
+*/
+
+//Pergunta 7
+/*
+let nome="Ramon";
+let sobreNome="Roman Mauri";
+console.log(nome + ' ' + sobreNome)
+*/
+
---
+++ ex4.js
@@ -0,0 +1,13 @@
+var nome="Carlos";
+var sobreNome="de Souza";
+var idade=28;
+var peso=80.1;
+var altura=1.76;
+var plano=true;
+var imc = null;
+
+imc = (peso) / (altura * altura);
+
+
+
+console.log(nome + " " + sobreNome + " tem " + idade + " anos e seu índice de massa corporal é de: " + imc);
